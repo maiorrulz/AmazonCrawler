85cf6bca6e8c3cc35039d7a0fc6f95df96402773
Alex <maiorrulz@gmail.com>
2017-01-25 20:26:28 +0200

    Fixes And shouldBeIgnored target folder

--- src/com/rest/crawler/Resource.java
+++ src/com/rest/crawler/Resource.java
@@ -2,21 +2,18 @@ package com.rest.crawler;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-//import javax.ws.rs.core.Response.Status;
-import com.rest.crawler.ReturnObject.Status;
 
 import org.json.JSONException;
-import org.json.JSONObject;
 
 import com.google.gson.Gson;
+//import javax.ws.rs.core.Response.Status;
+import com.rest.crawler.ReturnObject.Status;
 
 @Path("/amazonPages")
 public class Resource {
@@ -37,6 +34,7 @@ public class Resource {
 		else
 			result = "<h1>" + pc.getProductTitle() + "</h1>" 
 							+ "<img src="+pc.getImageUrl()+"> <hr>"
+							+"<a href=\"https://github.com/maiorrulz/AmazonCrawler\" style=\"margin-left: 300px\">Source GitHub</a>" 
 							+"<div>"+ new Gson().toJson(pc.processedProduct()) +"</div>";
 		
 	    return Response.status(200)
